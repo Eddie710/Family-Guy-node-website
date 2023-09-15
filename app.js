d1a3ebc8fa7bcff1f43eafdd8e0240b5b0c49c52
Eddie710 <111469943+Eddie710@users.noreply.github.com>
2023-09-15 14:27:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/name/:name', (req, res) =>{
 
 const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
